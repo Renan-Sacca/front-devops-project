7e69c16c00b178cac9c13b09402dbe835d5f67b4
Renan Sacca <renangs2222@gmail.com>
2025-02-22 14:56:22 -0300

    Imagem de usuario email e nome

--- front-devops/src/components/Footer.jsx
+++ front-devops/src/components/Footer.jsx
@@ -1,11 +1,16 @@
 import styles from "./Footer.module.css"
 import logo from "./../../public/logoDevBlog-H.png"
+import userimg from "./../../public/user.svg"
+
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h3>Escreva sobre o que você tem interesse!</h3>
-      <p>Fatec Dev Team &copy; 2024</p>
+      <div>
+
+      </div>
+      <h3>Unimar Pós Graduação DevOps!</h3>
+      <p>Unimar Dev Team &copy; 2024</p>
       <img src={logo} alt="Logo do Developer Mini Blog" />
     </footer>
   );
